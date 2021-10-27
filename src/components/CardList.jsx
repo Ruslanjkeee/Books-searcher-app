dbe879ffa7ef7fb8f45d37fece2d1f8adcd4167c
Ruslanjkeee <Pun1-@yandex.ru>
2021-10-27 16:37:28 +0300

    refactor CardList

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import CardItem from './CardItem';
 import MyButton from './UI/button/MyButton';
 import Loader from './UI/Loader/Loader';
@@ -6,33 +6,55 @@ import { observer } from 'mobx-react-lite';
 import bookStore from '../stores/bookStore';
 import cardsStore from '../stores/cardsStore';
 import startSettingStore from '../stores/startSettingStore';
+import BookService from '../API/BookService';
+import { useFetching } from '../hooks/useFetching';
+import FetchError from './FetchError';
 
-const CardList = observer(({fetchBookById, loadMore, isLoading}) => {
+const CardList = observer(({fetchError}) => {
 
-    function delegateClickHandler(e) {
-        let card = e.target.closest('.card');
-        if(card) {
-            bookStore.setBookId(card.id);
-            bookStore.setShowBookInfo(true);
-            fetchBookById();
+    const [loadMore, isMoreBooksLoading, fetchMoreError] = useFetching(async () => {
+        cardsStore.setStartIndex(cardsStore.startIndex + startSettingStore.pagStep);
+        let books = await BookService.getAll();
+       
+        if(books.totalItems && books.items) {
+            cardsStore.setTotalCards(books.totalItems);
+            cardsStore.updateCards([...cardsStore.cards, ...books.items]);
         }
-    }
+    })
 
+    const handleClick = useCallback(
+        (e) => {
+            let card = e.target.closest('.card');
+            if(card) {
+                bookStore.setBookId(card.id);
+                bookStore.setShowBookInfo(true);
+            }
+    }, []);
+
+    if(fetchMoreError || fetchError) {
+        return (
+            <FetchError
+                fetchMoreError={fetchMoreError}
+                fetchError={fetchError} 
+            />
+        )
+    }
+   
     return (
         <div className="main"> 
             <p className='main_results'>Found {cardsStore.totalCards} results</p>
             {cardsStore.cards.length > 0 &&
-                <div className="cards" onClick={delegateClickHandler}>
+                <div className="cards" onClick={handleClick}>
                     {cardsStore.cards.map((card, index) =>
                         <CardItem card={card} key={card.id + index}/>    
                     )}
                 </div>
             }
-            {isLoading
+            {isMoreBooksLoading
                 ? <Loader/>
                 : cardsStore.totalCards > cardsStore.startIndex + startSettingStore.pagStep
                     ? <MyButton onClick={loadMore} className="load-cards">Load more</MyButton>
-                    : ''
+                    : null
             }
         </div>
     )
